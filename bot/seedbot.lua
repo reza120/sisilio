5c4ba9fff1646feb00324fe2fc270b5f31f7b07b
reza120 <moohamdtatlari@gmail.com>
2016-07-02 20:22:11 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {162193322},--Sudo users
+    sudo_users = {162193322},--Sudo users{162193322}
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
